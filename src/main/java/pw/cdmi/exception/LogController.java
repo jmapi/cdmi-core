d61ae8b499b506568debe8928179c2218289e592
alpeai <alpeai@126.com>
2018-03-14 19:38:48 +0800

    取消Logback的引用操作，以便在其他项目中与其他log处理系统冲突

--- src/main/java/pw/cdmi/exception/LogController.java
+++ src/main/java/pw/cdmi/exception/LogController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.LoggerContext;
+//import ch.qos.logback.classic.Level;
+//import ch.qos.logback.classic.LoggerContext;
 
 @Controller
 public class LogController {
@@ -17,12 +17,12 @@ public class LogController {
 	
 	@RequestMapping(value="/log/{level}", method = RequestMethod.GET)
 	public void changeLogLevel(@PathVariable String level){
-		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-		ch.qos.logback.classic.Logger log = lc.getLogger(Logger.ROOT_LOGGER_NAME);
-		
-		logger.info("info: current log level is [" + log.getLevel().toString() + "]");
-		ch.qos.logback.classic.Level l = Level.toLevel(level);
-		log.setLevel(l);
-		logger.debug("debug: log level changed, now is [" + l.toString() + "]");
+//		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+//		ch.qos.logback.classic.Logger log = lc.getLogger(Logger.ROOT_LOGGER_NAME);
+//		
+//		logger.info("info: current log level is [" + log.getLevel().toString() + "]");
+//		ch.qos.logback.classic.Level l = Level.toLevel(level);
+//		log.setLevel(l);
+//		logger.debug("debug: log level changed, now is [" + l.toString() + "]");
 	}
 }
